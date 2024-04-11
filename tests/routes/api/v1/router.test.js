9355bbf05364b41657c9552368030989855f8c6e
Fredrik Svensson <fs222id@student.lnu.se>
2024-04-11 11:43:46 +0200

    Add tests for GET /nonexistent and HATEOAS Middleware

--- tests/routes/api/v1/router.test.js
+++ tests/routes/api/v1/router.test.js
@@ -6,8 +6,22 @@ const app = express()
 app.use(router)
 
 describe('Routes', () => {
+  it('GET /nonexistent should respond with a 404', async () => {
+    const response = await request(app).get('/nonexistent')
+    expect(response.status).toBe(404)
+  })
+  
   it('GET / should respond with a 200', async () => {
     const response = await request(app).get('/')
     expect(response.status).toBe(200)
+  })
+})
+
+describe('HATEOAS Middleware', () => {
+  it('should add links to the response', async () => {
+    const response = await request(app).get('/')
+    expect(response.status).toBe(200)
+    expect(response.body._links).toBeDefined()
+    expect(response.body._links.self).toBeDefined()
   })
 })
