6a72636d447a7f195f7eabaf9e4440dad2e1cee1
Fredrik Svensson <fs222id@student.lnu.se>
2024-04-13 14:20:02 +0200

    Update Babel configuration to include TypeScript preset

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,4 +1,7 @@
 module.exports = {
-  presets: [['@babel/preset-env', { targets: { node: 'current' }, modules: 'commonjs' }]],
+  presets: [
+    ['@babel/preset-env', { targets: { node: 'current' } }],
+    '@babel/preset-typescript'
+  ],
   plugins: ['transform-import-meta']
 }
