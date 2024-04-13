4f5d5a7c5ab05c0a5f1b03c69f6f2141652df671
Fredrik Svensson <fs222id@student.lnu.se>
2024-04-13 15:54:14 +0200

    Refactor router.js to use TypeScript syntax and handle HTTP errors

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -6,7 +6,8 @@
  * @since 0.1.0
  */
 
-import express from 'express'
+import express, { Request, Response, NextFunction } from 'express'
+import { HttpError } from '../lib/httpError'
 import { router as v1Router } from './api/v1/router.js'
 
 export const router = express.Router()
@@ -14,16 +15,15 @@ export const router = express.Router()
 router.use('/api/v1', v1Router)
 
 // Route for testing purposes. TODO: Remove before production.
-router.get('/', (req, res) => {
+router.get('/', (req: Request, res: Response) => {
   res.status(200).json({ message: 'Welcome, but this is not the API your looking for...' })
 })
-router.get('/error', (req, res) => {
+router.get('/error', (req: Request, res: Response) => {
   res.status(500).end()
 })
 
 // Error handling
-router.use('*', (req, res, next) => {
-  const error = new Error('Not Found')
-  error.status = 404
+router.use('*', (req: Request, res: Response, next: NextFunction) => {
+  const error = new HttpError('Not Found', 404)
   next(error)
 })
