cb93064f32f8800aa52e0b98b9f53b89656aa31a
Fredrik Svensson <fs222id@student.lnu.se>
2024-04-13 14:20:21 +0200

    Update Jest configuration to use TypeScript with ESM modules and transform ignore patterns

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,4 +1,24 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'node',
+  globals: {
+    'ts-jest': {
+      useESM: true,
+    }
+  },
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+    '^(\\.{1,2}/.*)\\.ts$': '$1',
+  },
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  extensionsToTreatAsEsm: ['.ts'],
   collectCoverage: true,
   coverageReporters: ['html'],
+  transformIgnorePatterns: [
+    "node_modules/(?!(module-that-needs-transpiling)/)" 
+  ]
 }
