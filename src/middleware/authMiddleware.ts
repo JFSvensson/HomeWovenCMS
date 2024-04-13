f54aac7a20212a72ca591e284275adf1dc1d3749
Fredrik Svensson <fs222id@student.lnu.se>
2024-04-13 19:45:17 +0200

    Refactor authMiddleware.js to use TypeScript syntax and handle unauthorized requests

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -5,7 +5,8 @@
  * @version 0.1.0
  * @since 0.1.0
  */
-
+import { Response, NextFunction } from 'express'
+import { Request } from '../interfaces/request'
 import jwt from 'jsonwebtoken'
 import { tokenBlacklist } from '../config/tokenBlacklist.js'
 
@@ -16,18 +17,24 @@ export class AuthMiddleware {
    * @param {Request} req - The request object.
    * @param {Response} res - The response object.
    * @param {NextFunction} next - The next middleware function.
-   * @returns {void}
    * @throws {Error} Throws an error if the token is blacklisted.
    */
-  checkAuthorization(req, res, next) {
+  checkAuthorization(req: Request, res: Response, next: NextFunction) {
     // Extract the token from the Authorization header ("Bearer <token>"").
-    const token = req.headers.authorization.split(' ')[1]
+    const token = req.headers.authorization?.split(' ')[1]
+    if (!token) {
+      return res.status(401).json({ message: 'Unauthorized' })
+    }
     if (tokenBlacklist.isListed(token)) {
       return res.status(401).json({ message: 'Token is blacklisted' })
     }
 
     try {
-      const userData= jwt.verify(token, process.env.ACCESS_TOKEN_SECRET.replace(/\\n/g, '\n'), { algorithms: ['RS256'] })
+      const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET
+      if (!accessTokenSecret) {
+        throw new Error('ACCESS_TOKEN_SECRET is not set')
+      }
+      const userData = jwt.verify(token, accessTokenSecret.replace(/\\n/g, '\n'), { algorithms: ['RS256'] })
       req.user = userData // Add the user data to the request object for use in other middleware functions.
       next()
     } catch (error) {
