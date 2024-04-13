e10df28c808dc31cdeb6ed3cb5d80f5c83f1d668
Fredrik Svensson <fs222id@student.lnu.se>
2024-04-13 20:14:47 +0200

    Add checkOwnerMiddleware.ts to handle authorization for resource ownership

---
+++ src/middleware/checkOwnerMiddleware.ts
@@ -0,0 +1,34 @@
+/**
+ * @file Defines the check owner middleware for the application.
+ * @module middleware
+ * @author Fredrik Svensson
+ * @version 0.1.0
+ * @since 0.1.0
+ */
+
+import { Response, NextFunction } from 'express'
+import { Request } from '../interfaces/request'
+
+export class CheckOwnerMiddleware {
+
+  /**
+   * Middleware to check if the request is authorized.
+   * If the user is not the owner of the requested resource, a 403 Forbidden response is sent.
+   * @param {Request} req - The request object.
+   * @param {Response} res - The response object.
+   * @param {NextFunction} next - The next middleware function.
+   * @returns {void}
+   */
+  async checkOwner(req: Request, res: Response, next: NextFunction) {
+
+    const isOwner = req.user && this.isOwner(req.user.sub, req.params.id)
+    if (!isOwner) {
+      return res.status(403).json({ message: 'Forbidden: You can only access your own data' })
+    }
+    next()
+  }
+
+  isOwner(userIdFromToken: any, userIdFromResource: any) {
+    return userIdFromToken.toString() === userIdFromResource.toString()
+  }
+}
