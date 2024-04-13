771e9ad6fe8e2a9491fa4d8ef8c622a9af4876d7
Fredrik Svensson <fs222id@student.lnu.se>
2024-04-14 00:10:46 +0200

    Refactor authRouter.js to use TypeScript syntax and update API routes

--- tests/routes/api/v1/authRouter.test.js
+++ tests/routes/api/v1/authRouter.test.js
@@ -1,24 +1,41 @@
 import request from 'supertest'
-import createServer from '../../../../src/server.js'
+import createServer from '../../../../src/server'
 import dotenv from 'dotenv'
 import express from 'express'
 import cookieParser from 'cookie-parser'
-import { disconnectFromDatabase } from '../../../../src/config/mongoose.js'
-import { User } from '../../../../src/models/user.js'
-import { router } from '../../../../src/routes/api/v1/authRouter.js'
-import { tokenBlacklist } from '../../../../src/config/tokenBlacklist.js'
+import { disconnectFromDatabase } from '../../../../src/config/mongoose'
+import { User } from '../../../../src/models/user'
+import { router } from '../../../../src/routes/api/v1/authRouter'
+import { tokenBlacklist } from '../../../../src/config/tokenBlacklist'
 
 dotenv.config({ path: '.env.test' })
 
-let app
-
-beforeAll(async () => {
+beforeEach(async () => {
   app = await createServer()
   app = express()
   app.use(cookieParser())
   app.use(express.json())
   app.use(router)
 })
+
+function getNewUserData() {
+  return {
+    username: 'testusername',
+    passphrase: 'testpassphrase',
+    firstName: 'Test',
+    lastName: 'User',
+    email: 'test@test.com'
+  }
+}
+
+const createUserAndLogin = async (app) => {
+  const userData = getNewUserData()
+  await request(app).post('/register').send(userData)
+  return request(app).post('/login').send({
+    username: userData.username,
+    passphrase: userData.passphrase
+  })
+}
 
 describe('Routes', () => {
   it('GET /nonexistent should respond with a 404', async () => {
@@ -37,60 +54,38 @@ describe('Routes', () => {
   })
 
   it('POST /register should respond with a 201 for valid data', async () => {
-    const data = {
-      username: 'testusername',
-      passphrase: 'testpassphrase',
-      firstName: 'Test',
-      lastName: 'User',
-      email: 'test@test.com'
-    }
-    const response = await request(app).post('/register').send(data)
+    const response = await request(app).post('/register').send(getNewUserData())
     expect(response.status).toBe(201)
   })
 
   it('POST /register should respond with a 409 for duplicate data', async () => {
-    const data = {
-      username: 'testusername',
-      passphrase: 'testpassphrase',
-      firstName: 'Test',
-      lastName: 'User',
-      email: 'test@test.com'
-    }
-    const response = await request(app).post('/register').send(data)
+    const userData = getNewUserData()
+    let response = await request(app).post('/register').send(userData)
+    response = await request(app).post('/register').send(userData)
     expect(response.status).toBe(409)
   })
 
   it('POST /register should respond with a 400 for too short passphrase', async () => {
-    const data = {
-      username: 'testusername',
-      passphrase: 'short',
-      firstName: 'Test',
-      lastName: 'User',
-      email: 'test@test.com'
-    }
-    const response = await request(app).post('/register').send(data)
+    const userData = getNewUserData()
+    userData.passphrase = 'short'
+    const response = await request(app).post('/register').send(userData)
     expect(response.status).toBe(400)
   })
 
   it('POST /register should respond with a 400 for invalid email', async () => {
-    const data = {
-      username: 'testusername',
-      passphrase: 'testpassphrase',
-      firstName: 'Test',
-      lastName: 'User',
-      email: 'nomail'
-    }
-    const response = await request(app).post('/register').send(data)
+    const userData = getNewUserData()
+    userData.email = 'nomail'
+    const response = await request(app).post('/register').send(userData)
     expect(response.status).toBe(400)
   })
 
-  it('POST /login should respond with a 200 for valid data', async () => {
-    const data = {
-      username: 'testusername',
-      passphrase: 'testpassphrase'
-    }
-    const response = await request(app).post('/login').send(data)
+  it('POST /login should respond with a 200 and tokens for valid data', async () => {
+    const response = await createUserAndLogin(app)
     expect(response.status).toBe(200)
+  
+    // Check the response body
+    expect(response.body).toHaveProperty('access_token')
+    expect(response.body).toHaveProperty('refresh_token')
   })
 
   it('POST /login should respond with a 401 for invalid username', async () => {
@@ -113,11 +108,7 @@ describe('Routes', () => {
 
   it('POST /refresh should respond with a 200 and a new token for valid data', async () => {
     // First, log in to get the tokens
-    const loginData = {
-      username: 'testusername',
-      passphrase: 'testpassphrase'
-    }
-    const loginResponse = await request(app).post('/login').send(loginData)
+    const loginResponse = await createUserAndLogin(app)
     expect(loginResponse.status).toBe(200)
   
     // Extract the tokens from the login response
@@ -136,11 +127,7 @@ describe('Routes', () => {
 
   it('POST /refresh should respond with a 403 for blacklisted token', async () => {
     // First, log in to get the tokens
-    const loginData = {
-      username: 'testusername',
-      passphrase: 'testpassphrase'
-    }
-    const loginResponse = await request(app).post('/login').send(loginData)
+    const loginResponse = await createUserAndLogin(app)
     expect(loginResponse.status).toBe(200)
   
     // Extract the tokens from the login response
@@ -161,11 +148,7 @@ describe('Routes', () => {
 
   it('POST /refresh should respond with a 401 for missing refresh token', async () => {
     // First, log in to get the tokens
-    const loginData = {
-      username: 'testusername',
-      passphrase: 'testpassphrase'
-    }
-    const loginResponse = await request(app).post('/login').send(loginData)
+    const loginResponse = await createUserAndLogin(app)
     expect(loginResponse.status).toBe(200)
   
     // Extract the tokens from the login response
@@ -182,11 +165,7 @@ describe('Routes', () => {
 
   it('POST /refresh should respond with a 403 for invalid refresh token', async () => {
     // First, log in to get the tokens
-    const loginData = {
-      username: 'testusername',
-      passphrase: 'testpassphrase'
-    }
-    const loginResponse = await request(app).post('/login').send(loginData)
+    const loginResponse = await createUserAndLogin(app)
     expect(loginResponse.status).toBe(200)
   
     // Extract the tokens from the login response
@@ -207,11 +186,7 @@ describe('Routes', () => {
 
   it('POST /logout should respond with a 200 for valid data', async () => {
     // First, log in to get a token
-    const loginData = {
-      username: 'testusername',
-      passphrase: 'testpassphrase'
-    }
-    const loginResponse = await request(app).post('/login').send(loginData)
+    const loginResponse = await createUserAndLogin(app)
     expect(loginResponse.status).toBe(200)
   
     // Extract the tokens from the login response
@@ -229,7 +204,7 @@ describe('Routes', () => {
 
 })
 
-afterAll(async () => {
+afterEach(async () => {
   await User.deleteMany({})
   await disconnectFromDatabase()
 })
