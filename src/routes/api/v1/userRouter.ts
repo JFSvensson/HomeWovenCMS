f24d7fe4b4138362a098f24ba8fed1b11ef42be9
Fredrik Svensson <fs222id@student.lnu.se>
2024-04-14 10:22:55 +0200

    Refactor to use dependency injection and update API routes

--- src/routes/api/v1/userRouter.ts
+++ src/routes/api/v1/userRouter.ts
@@ -5,116 +5,137 @@
  * @version 0.1.0
  * @since 0.1.0
  */
-
 import express from 'express'
-import { UsersController } from '../../../controllers/api/v1/userController.js'
-import { AuthMiddleware } from '../../../middleware/authMiddleware.js'
-import { CheckOwnerMiddleware } from '../../../middleware/checkOwnerMiddleware.js'
-import { HateoasMiddleware } from '../../../middleware/hateoasMiddleware.js'
+import { inject, injectable } from 'inversify'
+import { TYPES } from '../../../types'
+import { UserController } from '../../../controllers/api/v1/userController'
+import { AuthMiddleware } from '../../../middleware/authMiddleware'
+import { CheckOwnerMiddleware } from '../../../middleware/checkOwnerMiddleware'
+import { HateoasMiddleware } from '../../../middleware/hateoasMiddleware'
 
-export const router = express.Router()
+@injectable()
+export class UserRouter {
+  private usersController: UserController
+  private authMiddleware: AuthMiddleware
+  private checkOwnerMiddleware: CheckOwnerMiddleware
+  private hateoasMiddleware: HateoasMiddleware
 
-const controller = new UsersController()
-const checkAuthorization = new AuthMiddleware()
-const checkOwner = new CheckOwnerMiddleware()
-const hateoas = new HateoasMiddleware()
+  constructor(
+    @inject(TYPES.UserController) usersController: UserController,
+    @inject(TYPES.AuthMiddleware) authMiddleware: AuthMiddleware,
+    @inject(TYPES.CheckOwnerMiddleware) checkOwnerMiddleware: CheckOwnerMiddleware,
+    @inject(TYPES.HateoasMiddleware) hateoasMiddleware: HateoasMiddleware
+  ) {
+    this.usersController = usersController
+    this.authMiddleware = authMiddleware
+    this.checkOwnerMiddleware = checkOwnerMiddleware
+    this.hateoasMiddleware = hateoasMiddleware
+  }
 
-/**
- * @openapi
- * /users/{id}:
- *   get:
- *     summary: Get information about a specific user
- *     description: Returns information about a specific user in the system.
- *     tags:
- *       - Users
- *     parameters:
- *       - name: id
- *         in: path
- *         required: true
- *         schema:
- *           type: string
- *         description: The user's ID.
- *     responses:
- *       '200':
- *         description: Successful response with user information.
- *         content:
- *           application/json:
- *             schema:
- *               $ref: '#/components/schemas/User'
- *       '404':
- *         description: User not found.
- */
-router.get(
-  '/:id', 
-  checkAuthorization.checkAuthorization.bind(checkAuthorization), 
-  checkOwner.checkOwner.bind(checkOwner),
-  hateoas.addLinks,
-  (req, res) => controller.getUser(req, res)
-)
+  getRouter(): express.Router {
+    const router = express.Router()
 
- /**
- * @openapi
- * /users/{id}:
- *   put:
- *    summary: Update information about a specific user
- *    description: Updates information about a specific user in the system.
- *    tags:
- *      - Users
- *    parameters:
- *      - name: id
- *        in: path
- *        required: true
- *        schema:
- *          type: string
- *        description: The user's ID.
- *    requestBody:
- *      required: true
- *      content:
- *        application/json:
- *          schema:
- *            $ref: '#/components/schemas/NewUser'
- *    responses:
- *      '200':
- *        description: User updated successfully.
- *      '404':
- *        description: User not found.
- */
-router.put(
-  '/:id', 
-  checkAuthorization.checkAuthorization.bind(checkAuthorization),
-  checkOwner.checkOwner.bind(checkOwner),
-  hateoas.addLinks,
-  (req, res) => controller.updateUser(req, res)
-)
 
-/**
- * @openapi
- * /users/{id}:
- *   delete:
- *     summary: Delete a specific user
- *     description: Deletes a specific user from the system.
- *     tags:
- *      - Users
- *     parameters:
- *       - name: id
- *         in: path
- *         required: true
- *         schema:
- *           type: string
- *         description: The user's ID.
- *     responses:
- *       '200':
- *         description: User deleted successfully.
- *       '404':
- *         description: User not found.
- */
-router.delete(
-  '/:id', 
-  checkAuthorization.checkAuthorization.bind(checkAuthorization),
-  checkOwner.checkOwner.bind(checkOwner),
-  hateoas.addLinks,
-  (req, res) => controller.deleteUser(req, res)
-)
+    /**
+     * @openapi
+     * /users/{id}:
+     *   get:
+     *     summary: Get information about a specific user
+     *     description: Returns information about a specific user in the system.
+     *     tags:
+     *       - Users
+     *     parameters:
+     *       - name: id
+     *         in: path
+     *         required: true
+     *         schema:
+     *           type: string
+     *         description: The user's ID.
+     *     responses:
+     *       '200':
+     *         description: Successful response with user information.
+     *         content:
+     *           application/json:
+     *             schema:
+     *               $ref: '#/components/schemas/User'
+     *       '404':
+     *         description: User not found.
+     */
+    router.get(
+      '/:id', 
+      this.authMiddleware.checkAuthorization.bind(this.authMiddleware), 
+      this.checkOwnerMiddleware.checkOwner.bind(this.checkOwnerMiddleware),
+      this.hateoasMiddleware.addLinks,
+      (req, res) => this.usersController.getUser(req, res)
+    )
+
+    /**
+     * @openapi
+     * /users/{id}:
+     *   put:
+     *    summary: Update information about a specific user
+     *    description: Updates information about a specific user in the system.
+     *    tags:
+     *      - Users
+     *    parameters:
+     *      - name: id
+     *        in: path
+     *        required: true
+     *        schema:
+     *          type: string
+     *        description: The user's ID.
+     *    requestBody:
+     *      required: true
+     *      content:
+     *        application/json:
+     *          schema:
+     *            $ref: '#/components/schemas/NewUser'
+     *    responses:
+     *      '200':
+     *        description: User updated successfully.
+     *      '404':
+     *        description: User not found.
+     */
+    router.put(
+      '/:id', 
+      this.authMiddleware.checkAuthorization.bind(this.authMiddleware), 
+      this.checkOwnerMiddleware.checkOwner.bind(this.checkOwnerMiddleware),
+      this.hateoasMiddleware.addLinks,
+      (req, res) => this.usersController.updateUser(req, res)
+    )
+
+    /**
+     * @openapi
+     * /users/{id}:
+     *   delete:
+     *     summary: Delete a specific user
+     *     description: Deletes a specific user from the system.
+     *     tags:
+     *      - Users
+     *     parameters:
+     *       - name: id
+     *         in: path
+     *         required: true
+     *         schema:
+     *           type: string
+     *         description: The user's ID.
+     *     responses:
+     *       '200':
+     *         description: User deleted successfully.
+     *       '404':
+     *         description: User not found.
+     */
+    router.delete(
+      '/:id', 
+      this.authMiddleware.checkAuthorization.bind(this.authMiddleware), 
+      this.checkOwnerMiddleware.checkOwner.bind(this.checkOwnerMiddleware),
+      this.hateoasMiddleware.addLinks,
+      (req, res) => this.usersController.deleteUser(req, res)
+    )
+
+    return router
+  }
+}
 
 /**
  * @openapi
