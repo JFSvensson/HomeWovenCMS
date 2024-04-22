e60a7f15e38fef8484d6e77900af6fc3803174c0
Fredrik Svensson <fs222id@student.lnu.se>
2024-04-22 14:09:39 +0200

    Add ArticleService for handling article operations

---
+++ src/services/api/v1/articleService.ts
@@ -0,0 +1,31 @@
+/**
+ * @file Defines the article service for the application.
+ * @module service
+ * @author Fredrik Svensson
+ * @version 0.1.0
+ * @since 0.1.0
+ */
+
+import { Article } from '../../../models/article.js'
+
+export class ArticleService {
+  async getUserById(id: any) {
+    const article = await Article.findById(id).select('-username -passphrase')
+    return article
+  }
+
+  async createArticle(data: any) {
+    const article = await Article.create(data)
+    return { message: 'Article created successfully', article }
+  }
+
+  async updateArticle(id: any, data: any) {
+    const article = await Article.findByIdAndUpdate(id, data, { new: true, runValidators: true })
+    return { message: 'Article updated successfully', article }
+  }
+
+  async deleteArticle(id: any) {
+    const article = await Article.findByIdAndDelete(id)
+    return { message: 'Article deleted successfully', article }
+  }
+}
