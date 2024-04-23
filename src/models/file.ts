60542d7671eb66ae7ac8c38599521a16581c0cf1
Fredrik Svensson <fs222id@student.lnu.se>
2024-04-23 13:14:54 +0200

    Add File model for managing file data

---
+++ src/models/file.ts
@@ -0,0 +1,51 @@
+/**
+ * @file This file defines the File model.
+ * @author Fredrik Svensson
+ * @version 0.1.0
+ * @since 0.1.0
+ */
+
+import mongoose from 'mongoose'
+import validator from 'validator'
+import { IFile } from '../interfaces/file.js'
+import { IFileModel } from '../interfaces/fileModel.js'
+
+const { isURL } = validator
+
+// Create a schema.
+const schema = new mongoose.Schema({
+  url: {
+    type: String,
+    required: [true, 'Image URL is required.'],
+    validate: [isURL, 'Please provide a valid URL for the image.']
+  },
+  description: {
+    type: String,
+    required: [true, 'Description is required.'],
+    trim: true,
+    minLength: [5, 'The title must be of minimum length 5 characters.'],
+    maxLength: [1024, 'The title must be of maximum length 512 characters.']
+  }
+}, {
+  timestamps: true,
+  toJSON: {
+    /**
+     * Performs a transformation of the resulting object to ensure sensitive information is not serialized.
+     *
+     * @param {object} doc - The mongoose document which is being converted.
+     * @param {object} ret - The plain object representation which has been converted.
+     */
+    transform: function (doc, ret) {
+      delete ret._id
+      delete ret.__v
+    },
+    virtuals: true // ensure virtual fields are serialized
+  }
+})
+
+schema.virtual('id').get(function () {
+  return this._id.toHexString()
+})
+
+// Create a model using the schema.
+export const File = mongoose.model<IFile, IFileModel>('File', schema)
