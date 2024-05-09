63480a325501c77b649de0d88837c397278d12f7
Fredrik Svensson <fs222id@student.lnu.se>
2024-05-09 12:19:21 +0200

    Add IUser to Request user

--- src/interfaces/request.ts
+++ src/interfaces/request.ts
@@ -1,6 +1,7 @@
 import { Request as ExpressRequest } from 'express'
 import { JwtPayload } from "jsonwebtoken"
+import { IUser } from './user.js'
 
 export interface Request extends ExpressRequest {
-  user?: string | JwtPayload
+  user?: string | JwtPayload | IUser
 }
